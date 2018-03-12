4a875e668853170c17626db24a2eb9c4ba231494
Nishimwe Robert <nzeroberto@gmail.com>
2018-03-12 14:17:57 +0000

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,5 @@
 //= require rails-ujs
 //= require turbolinks
 //= require jquery/dist/jquery.js
-//= require bootstrap/dist/js/bootstrap.js
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
